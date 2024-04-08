28684833399c7e08525a1f1179127345f35391cd
Mateus Madeira <mafemad2005@gmail.com>
2024-04-08 19:39:28 -0300

    entity user

---
+++ src/main/java/com/projeto/springMongoDB/springmongo/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.projeto.springMongoDB.springmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto.springMongoDB.springmongo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		User maria = new User("1", "maria brown", "maria@email.com");
+		User alex = new User("2", "alex green", "alex@email.com");
+		
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+}
