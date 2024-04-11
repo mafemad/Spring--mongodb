5a769b6208a15b51570458fcd359c18ba66352a1
Mateus Madeira <mafemad2005@gmail.com>
2024-04-11 18:41:42 -0300

    seeding banco atraves de config

---
+++ src/main/java/com/projeto/springMongoDB/springmongo/config/Instatiation.java
@@ -0,0 +1,29 @@
+package com.projeto.springMongoDB.springmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.projeto.springMongoDB.springmongo.entities.User;
+import com.projeto.springMongoDB.springmongo.repository.UserRepository;
+
+@Configuration
+public class Instatiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepo;
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepo.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepo.saveAll(Arrays.asList(maria,alex,bob));
+	}
+
+}
