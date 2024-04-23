affae451c9f474ab953021b2eb09bf0919af5c72
Mateus Madeira <mafemad2005@gmail.com>
2024-04-23 19:46:00 -0300

    add comentarios

--- src/main/java/com/projeto/springMongoDB/springmongo/config/Instatiation.java
+++ src/main/java/com/projeto/springMongoDB/springmongo/config/Instatiation.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.projeto.springMongoDB.springmongo.DTO.AuthorDTO;
+import com.projeto.springMongoDB.springmongo.DTO.CommentDTO;
 import com.projeto.springMongoDB.springmongo.entities.Post;
 import com.projeto.springMongoDB.springmongo.entities.User;
 import com.projeto.springMongoDB.springmongo.repository.PostRepository;
@@ -39,6 +40,12 @@ public class Instatiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2024"), "partiu viagem", "Vou para caçapava, lessss go", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("21/04/2024"), "adoro comer sushi", "sushi e sashimi sao as melhores coisas",new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("boa viagem mano!", sdf.parse("25/03/2024"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("aproveite!", sdf.parse("22/03/2024"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("boa viagem mano!", sdf.parse("25/03/2024"), new AuthorDTO(maria));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepo.saveAll(Arrays.asList(post1,post2));
 		
--- src/main/java/com/projeto/springMongoDB/springmongo/entities/Post.java
+++ src/main/java/com/projeto/springMongoDB/springmongo/entities/Post.java
@@ -1,13 +1,16 @@
 package com.projeto.springMongoDB.springmongo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.projeto.springMongoDB.springmongo.DTO.AuthorDTO;
+import com.projeto.springMongoDB.springmongo.DTO.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +23,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	
 	public Post() {
@@ -82,10 +87,18 @@ public class Post implements Serializable{
 	}
 
 
-	public void setAuthor(AuthorDTO author) {
+	public void setAuthor(AuthorDTO author) {	
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -104,6 +117,6 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 	
 }
